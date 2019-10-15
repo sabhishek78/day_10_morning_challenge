60109d3f4482c17b2fa7bdc4f492735eaa4b93ec
abhishek shrivastava <abhishekshrivastava78@gmail.com>
2019-10-15 17:10:51 +0530

    test passed

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,7 @@
+import 'dart:math';
+import 'dart:io';
+
+
 // Challenge 1
 // Write a function that returns the longest sequence of consecutive zeroes in a binary string.
 // Examples:
@@ -7,6 +11,35 @@
 //
 // longestZero("11111") ➞ ""
 
+String longestZero(String binary)
+{
+  int count=0;
+  int max=0;
+  int zeroStringEnds;
+  List<String> zeroList=[];
+ for(int i=0;i<binary.length;i++)
+   {
+     if(binary[i]=='0')
+       {
+         count++;
+       }
+     else{
+       zeroStringEnds=1;
+       if(zeroStringEnds==1 && i!=0)
+         {
+           if(max<count)
+             {
+               max=count;
+               count=0;
+             }
+         }
+     }
+   }
+ //print(max);
+  zeroList =List<String>.filled(max,'0');
+  String answer=zeroList.join();
+  return answer;
+}
 
 // Challenge 2
 // Create a function that performs an even-odd transform to an array, n times. Each even-odd transformation:
@@ -21,8 +54,34 @@
 //
 // evenOddTransform([1, 2, 3], 1) ➞ [3, 0, 5]
 
-main() {
+List<int> evenOddTransform(List<int> array,int n)
+{
+  for(int i=0;i<array.length;i++)
+    {
+      /*if(array[i]<0)
+        {
+          print("Invalid Array");
+          return;
+        }*/
+      if(array[i] % 2==0)
+        {
+          array[i]=array[i]-2*n;
 
+        }
+      else{
+        array[i]=array[i]+2*n;
+      }
+    }
+  return array;
+}
+
+
+
+
+
+main() {
+ print(longestZero('10010001'));
+  print(evenOddTransform([3,4,9], 3));
 }
 
 // Challenge 3
--- bin/test.dart
+++ bin/test.dart
@@ -1,5 +1,16 @@
 import 'package:test/test.dart';
+import 'main.dart';
 
 void main(){
+  test('EvenOddTransformTest', (){
+    expect(evenOddTransform([1, 2, 3], 1,),[3, 0, 5]);
+    expect(evenOddTransform([3,4,9], 3,),[9, -2, 15]);
+    expect(evenOddTransform([0,0,0], 10),[-20, -20, -20]);
+  });
+  test('LongestZero', (){
 
+    expect(longestZero("101001"),"00");
+    expect(longestZero("01100001011000"),"0000");
+    expect(longestZero("11111"),"");
+  });
 }
